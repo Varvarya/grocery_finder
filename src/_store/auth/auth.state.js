a5ec5287361c905fbbfd220c056ed7b03c0db82b
Varvarya <varvara.tisheninova@nure.ua>
2024-05-27 22:14:20 +0300

    Changed base code

--- src/_store/auth/auth.state.js
+++
@@ -1,7 +0,0 @@
-export function createInitialState() {
-    return {
-        // initialize state from local storage to enable user to stay logged in
-        user: JSON.parse(localStorage.getItem('user')),
-        error: null
-    }
-}
